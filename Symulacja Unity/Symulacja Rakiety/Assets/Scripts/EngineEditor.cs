020a5130a91ff658d399dc6c3cb880e90f544cc2
Krzychu-Lab\Krzychu <dainarudd@gmail.com>
2017-11-11 16:26:09 +0100

    scena

--- Symulacja Unity/Symulacja Rakiety/Assets/Scripts/EngineEditor.cs
+++ Symulacja Unity/Symulacja Rakiety/Assets/Scripts/EngineEditor.cs
@@ -9,12 +9,19 @@ public class EngineEditor : MonoBehaviour
 	SpaceShuttleController controller;
 	public UnityEngine.UI.Dropdown ddMainEngines;
 	public UnityEngine.UI.Dropdown ddMainEnginesCount;
-	public UnityEngine.UI.Dropdown SRBsDropdown;
-	public UnityEngine.UI.Dropdown SRBsDropdownCount;
+	public UnityEngine.UI.Dropdown ddSRBs;
+	public UnityEngine.UI.Dropdown ddSRBsCount;
 
-	public GameObject enginePrefab;
+	public GameObject engineRD_170Prefab;
+	public GameObject engineRD_180Prefab;
+	public GameObject engineRS_25Prefab;
+	public GameObject engineRS_25DPrefab;
+	public GameObject SRB4_segmentPrefab;
+	public GameObject SRB5_segmentPrefab;
+	public GameObject selectedEngine;
+	public GameObject selectedSRB;
 
-	private int mec = 0;
+	private int mec = 1, srbc = 0;
 
 	private readonly Dictionary<int, string> dMainEngines = new Dictionary<int, string> ()
 	{
@@ -42,10 +49,9 @@ public class EngineEditor : MonoBehaviour
 	private readonly Dictionary<int, string> dSRBsCount = new Dictionary<int, string> ()
 	{
 		{0, "0"},
-		{1, "1"},
-		{2, "2"},
-		{3, "3"},
-		{4, "4"}
+		{1, "2"},
+		{2, "3"},
+		{3, "4"}
 	};
 
 	void Start ()
@@ -59,16 +65,19 @@ public class EngineEditor : MonoBehaviour
 		// engines number
 		ddMainEnginesCount.ClearOptions ();
 		ddMainEnginesCount.AddOptions (dMainEnginesCount.Values.ToList ());
-		ddMainEnginesCount.onValueChanged.AddListener (SpawnEngines);
+		ddMainEnginesCount.onValueChanged.AddListener (ChangeEnginesCount);
 
 		// srb list
-		SRBsDropdown.ClearOptions ();
-		SRBsDropdown.AddOptions (dSRBs.Values.ToList ());
+		ddSRBs.ClearOptions ();
+		ddSRBs.AddOptions (dSRBs.Values.ToList ());
 
 		//srb number
-		SRBsDropdownCount.ClearOptions ();
-		SRBsDropdownCount.AddOptions (dSRBsCount.Values.ToList ());
+		ddSRBsCount.ClearOptions ();
+		ddSRBsCount.AddOptions (dSRBsCount.Values.ToList ());
+		ddSRBsCount.onValueChanged.AddListener (ChangeSRBsCount);
 
+		selectedEngine = engineRD_170Prefab;
+		selectedSRB = SRB4_segmentPrefab;
 
 	}
 
@@ -77,7 +86,36 @@ public class EngineEditor : MonoBehaviour
 
 	}
 
-	public void SpawnEngines (int index)
+	public void ChangeEngines (int index)
+	{
+		switch (dMainEngines.Values.ElementAt (index))
+		{
+			case "RD_170":
+				selectedEngine = engineRD_170Prefab;
+				break;
+			case "RD_180":
+				selectedEngine = engineRD_180Prefab;
+				break;
+			case "RS_25":
+				selectedEngine = engineRS_25Prefab;
+				break;
+			case "RS_25D":
+				selectedEngine = engineRS_25DPrefab;
+				break;
+		}
+
+		for (int i=0; i < controller.engines.Count; i++)
+		{
+
+		}
+	}
+
+	public void ChangeSRBs (int index)
+	{
+
+	}
+
+	public void ChangeEnginesCount (int index)
 	{
 		mec = Convert.ToInt32 (dMainEnginesCount.Values.ElementAt (index));
 
@@ -86,14 +124,34 @@ public class EngineEditor : MonoBehaviour
 			Destroy (controller.engines [i].gameObject);
 		controller.engines.Clear ();
 
-		Vector3 distVec = Vector3.forward * 4f + 2.25f * Vector3.up;
+		Vector3 distVec = Vector3.forward * 4f;
 		Vector3 rotVec = Vector3.zero;
 		rotVec.y = 360 / mec;
 		for (int i = 0; i < mec; i++)
 		{
 			distVec = Quaternion.Euler (rotVec) * distVec;
 			print (distVec);
-			controller.engines.Add (Instantiate (enginePrefab, distVec, Quaternion.identity, controller.transform).GetComponent<Engine> ());
+			controller.engines.Add (Instantiate (selectedEngine, controller.transform.position + distVec, Quaternion.identity, controller.transform).GetComponent<Engine> ());
+		}
+	}
+
+	public void ChangeSRBsCount (int index)
+	{
+		srbc = Convert.ToInt32 (dSRBsCount.Values.ElementAt (index));
+
+
+		for (int i = 0; i < controller.engineBusters.Count; i++)
+			Destroy (controller.engineBusters [i].gameObject);
+		controller.engineBusters.Clear ();
+
+		Vector3 distVec = Vector3.forward * 6.5f;
+		Vector3 rotVec = Vector3.zero;
+		rotVec.y = 360 / srbc;
+		for (int i = 0; i < srbc; i++)
+		{
+			distVec = Quaternion.Euler (rotVec) * distVec;
+			print (distVec);
+			controller.engineBusters.Add (Instantiate (selectedSRB, controller.transform.position + distVec, Quaternion.identity, controller.transform).GetComponent<Engine> ());
 		}
 	}
 }
